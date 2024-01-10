2b31191637574507db848567c867f87b8409c7d3
ViniciusMendesp <vmp_tech@outlook.com>
2024-01-10 00:17:26 -0300

    fix: :bug: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
 
-export const fontSans = FontSans({
+export const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
