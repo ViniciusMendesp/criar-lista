8ad3ebe81605ac49981cd6dd03ba4c8e8b798d8d
ViniciusMendesp <vmp_tech@outlook.com>
2024-01-10 00:15:06 -0300

    feat: :art: criar lista app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,110 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Send, Trash } from "lucide-react";
+
+interface ListaItem {
+  nome: string;
+  quantidade: string;
+}
 
 export default function Home() {
+  const [nomeProduto, setNomeProduto] = useState<string>("");
+  const [quantidade, setQuantidade] = useState<string>("");
+  const [listaDeCompras, setListaDeCompras] = useState<ListaItem[]>([]);
+
+  const adicionarItem = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (nomeProduto && quantidade) {
+      const novoItem: ListaItem = { nome: nomeProduto, quantidade };
+      setListaDeCompras([...listaDeCompras, novoItem]);
+      setNomeProduto("");
+      setQuantidade("");
+    }
+  };
+
+  const removerItem = (index: number) => {
+    const novaLista = [...listaDeCompras];
+    novaLista.splice(index, 1);
+    setListaDeCompras(novaLista);
+  };
+
+  const enviarLista = (e: React.FormEvent) => {
+    e.preventDefault();
+    const mensagem = `LISTA DE COMPRAS%0A${listaDeCompras
+      .map((item) => `${item.nome} = ${item.quantidade}`)
+      .join("%0A")}`;
+    const url = `https://wa.me/?text=${mensagem}`;
+    window.open(url, "_blank");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col gap-4">
+      <header className="w-full p-4 flex items-center justify-between border-b">
+        <h1 className="font-bold text-lg">Criar Lista App</h1>
+        <ModeToggle />
+      </header>
+      <main className="p-4 flex flex-col gap-8">
+        <form className="flex flex-col gap-4">
+          <Input
+            placeholder="Nome do produto"
+            value={nomeProduto}
+            onChange={(e) => setNomeProduto(e.target.value)}
+          />
+          <Input
+            placeholder="Quantidade"
+            type="number"
+            value={quantidade}
+            onChange={(e) => setQuantidade(e.target.value)}
+          />
+          <Button onClick={adicionarItem}>Adicionar item à lista</Button>
+          <Button
+            variant="secondary"
+            className="flex gap-2"
+            onClick={enviarLista}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            Enviar lista <Send size={16} />
+          </Button>
+        </form>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Nome</TableHead>
+              <TableHead>Quantidade</TableHead>
+              <TableHead>Remover</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {listaDeCompras.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>{item.nome}</TableCell>
+                <TableCell>{item.quantidade}</TableCell>
+                <TableCell className="text-right">
+                  <Button
+                    size="icon"
+                    variant="destructive"
+                    onClick={() => removerItem(index)}
+                  >
+                    <Trash size={20} />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+    </div>
+  );
 }
